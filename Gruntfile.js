9aa5a4730401da39facf4510d6d994084f56a808
Karl Courtney <karlcourtney@gmail.com>
2015-08-08 14:53:01 +0100

    Intitial assemble set up

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,3 +1,24 @@
 module.exports = function(grunt){
 
+	grunt.initConfig({
+
+		assemble: {
+			options: {
+				flatten: true,
+				layoutdir: 'src/templates/layouts/', 
+				layout: 'default.hbs',
+				data: 'src/data/**/*.json',
+				partials: 'src/templates/partials/*.hbs'
+			},
+			site: {
+				files: {
+					'build/': ['src/templates/pages/*.hbs']
+				}
+			}
+		}
+	});
+
+	grunt.loadNpmTasks('assemble');
+
+	grunt.registerTask('default', ['assemble']);
 }
